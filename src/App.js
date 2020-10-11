1ff74d384321e50dc7c115d543b0273277a03578
Shuchintha Srinivasa <shuchinthasrinivasa@Shuchinthas-MacBook-Pro.local>
2020-10-11 11:09:44 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>helllo</h1>
+      <h1>Header</h1>
+      <h1>Title + select input dropdown</h1>
+      <h1>InfoBox</h1>
+      <h1>InfoBox</h1>
+      <h1>InfoBox</h1>
+      <h1>Table</h1>
+      <h1>Graph</h1>
+      <h1>map</h1>
     </div>
   );
 }
